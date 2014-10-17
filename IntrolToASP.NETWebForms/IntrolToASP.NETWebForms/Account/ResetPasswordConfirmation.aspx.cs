70da46beb10b66bcb402172ccbbe2a4dd9c6cd95
georgipyaramov <georgipyaramov@gmail.com>
2014-10-17 18:19:52 +0300

    Initial commit

---
+++ IntrolToASP.NETWebForms/IntrolToASP.NETWebForms/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace IntrolToASP.NETWebForms.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ IntrolToASP.NETWebForms/IntrolToASP.NETWebForms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntrolToASP.NETWebForms.Global" Language="C#" %>
