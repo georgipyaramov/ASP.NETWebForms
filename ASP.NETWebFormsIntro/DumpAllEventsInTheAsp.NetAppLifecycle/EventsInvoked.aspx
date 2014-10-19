236d4d942ee1569da08838618abb6907f66f7aa0
georgipyaramov <georgipyaramov@gmail.com>
2014-10-19 12:57:47 +0300

    Third task done

---
+++ ASP.NETWebFormsIntro/DumpAllEventsInTheAsp.NetAppLifecycle/EventsInvoked.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" 
+    AutoEventWireup="true" 
+    CodeBehind="EventsInvoked.aspx.cs" 
+    Inherits="DumpAllEventsInTheAsp.NetAppLifecycle.EventsInvoked" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button Text="Press me!" ID="btn" runat="server" 
+            OnClick="btn_Click" 
+            OnInit="btn_Init" 
+            OnLoad="btn_Load" 
+            OnPreRender="btn_PreRender" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NETWebFormsIntro/DumpAllEventsInTheAsp.NetAppLifecycle/EventsInvoked.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DumpAllEventsInTheAsp.NetAppLifecycle
+{
+    public partial class EventsInvoked : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Response.Write("Page PreInit invoked!" + "<br/>");
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Response.Write("Page Init invoked!" + "<br/>");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Page Load invoked!" + "<br/>");
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            Response.Write("Page PreRender invoked!" + "<br/>");
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            //Response is not avaiable in Unload
+        }
+
+
+        protected void btn_Init(object sender, EventArgs e)
+        {
+            Response.Write("Button Init invoked!" + "<br/>");
+        }
+
+        protected void btn_Load(object sender, EventArgs e)
+        {
+            Response.Write("Button Load invoked!" + "<br/>");
+        }
+
+        protected void btn_PreRender(object sender, EventArgs e)
+        {
+            Response.Write("Button PreRender invoked!" + "<br/>");
+        }
+
+        protected void btn_Unload(object sender, EventArgs e)
+        {
+            //Response is not avaiable in Unload
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            Response.Write("Button Click invoked!" + "<br/>");
+        }
+    }
+}
