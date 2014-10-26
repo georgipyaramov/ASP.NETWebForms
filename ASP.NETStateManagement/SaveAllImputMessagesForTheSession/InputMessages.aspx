16f08b02843de281ec7291cb7b00a7f1140847b5
georgipyaramov <georgipyaramov@gmail.com>
2014-10-26 23:27:45 +0200

    Second task done

---
+++ ASP.NETStateManagement/SaveAllImputMessagesForTheSession/InputMessages.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InputMessages.aspx.cs" Inherits="SaveAllImputMessagesForTheSession.InputMessages" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title></title>
+        <link href="Styles/bootstrap.min.css" rel="stylesheet" />
+        <link href="Styles/Site.css" rel="stylesheet" />
+    </head>
+    <body>
+        <form id="form1" runat="server" class="container form-horizontal">
+            <div class="form-group">
+                <asp:Label Text="Enter your message here" runat="server" AssociatedControlID="TextBoxInputMessages" />
+                <asp:TextBox runat="server" ID="TextBoxInputMessages" CssClass="form-control" />
+            </div>
+            <asp:Button ID="ButtonSaveMessage" Text="Save message" runat="server" CssClass="btn btn-primary" OnClick="ButtonSaveMessage_Click" />
+            <div id="MessagesContainer" runat="server" class="margin-top-10">
+
+            </div>
+        </form>
+    </body>
+</html>
---
+++ ASP.NETStateManagement/SaveAllImputMessagesForTheSession/InputMessages.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SaveAllImputMessagesForTheSession
+{
+    public partial class InputMessages : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void ButtonSaveMessage_Click(object sender, EventArgs e)
+        {
+            if (this.Session["inputMessages"] == null)
+            {
+                this.Session["inputMessages"] = new List<string>();
+            }
+
+            (this.Session["inputMessages"] as IList<string>).Add(this.TextBoxInputMessages.Text);
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            this.MessagesContainer.InnerHtml = "";
+
+            if (this.Session["inputMessages"] != null)
+            {
+                foreach (var message in (this.Session["inputMessages"] as IList<string>))
+                {
+                    this.MessagesContainer.InnerHtml += "<p>" + message + "</p>";                
+                }
+            }
+        }
+    }
+}
